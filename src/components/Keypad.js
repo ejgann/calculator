0cb640c0b786a072d561fd0edc3e6caa6588f0ff
Emily Gann <aspentree79@gmail.com>
2021-08-09 15:15:35 -0400

    added buttonValues nested array

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 
 class Keypad extends Component {
     render() {
+
+        let buttonValues = [
+            ['C', '+-', '%', '/'],
+            [7, 8, 9, 'X'],
+            [4, 5, 6, '-'],
+            [3, 2, 1, '+'],
+            [0, '.', '='],
+        ];
+
         return (
             <div>
                 
